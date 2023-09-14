698928477acbf294ca881d7d99937bb5d602bbb9
DHRUVIL BHOJANI <77846582+Dhruvilbhojani@users.noreply.github.com>
2023-09-14 12:36:08 +0530

    Add files via upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar';
+import { Box } from '@mui/material';
+
+export default function Home() {
+    return (
+        <>
+            <Navbar />
+            <Box>
+
+            </Box>
+        </>
+    )
+}
